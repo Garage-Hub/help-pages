39257293d4dc976851c2b0ecb8da827cef170cfa
Mattonit <malan95@gmail.com>
2019-04-25 15:16:42 +0200

    added some config for static files

--- help/urls.py
+++ help/urls.py
@@ -17,9 +17,14 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name="home.html"), name='home'),
     path('<str:category>/<slug:slug>', views.single_page.as_view(), name='single')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
